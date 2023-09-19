68dd322601454a1605e4843b5477f5c22d7983a0
carolinaaruiz <carolinaandrearuiz@gmail.com>
2023-09-19 15:04:09 -0300

    convertir CSV to Array

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#LOGICA GRAL
+import numpy as np
+import csv
+
+#convertir CSV to Array
+csv_filename = './dataset/dataset_1.csv'
+with open(csv_filename) as f:
+    reader = csv.reader(f)
+    next(reader)
+    lst = list(reader)
+    print(lst)
