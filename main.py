9b9f5cee29ae64dbef549a44095b19480dde22d5
carolinaaruiz <carolinaandrearuiz@gmail.com>
2023-09-20 17:24:43 -0300

    graficando todos los puntos de un dataset

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import csv
 import matplotlib.pyplot as plt
 
 #convertir CSV to Array text
-csv_filename = './dataset/dataset_1.csv'
+csv_filename = './dataset/dataset_3.csv'
 with open(csv_filename,newline='', encoding='utf-8') as f:
     reader = csv.reader(f,delimiter=';') 
     next(reader) #omite el encabezado
     array_csv_lista = list(reader)
     array_text_lista = np.array(array_csv_lista) #convierte lista en array
-   
-   #convertir x,y elemento a float
-    x = array_text_lista[0][0]
-    x_float = float(x.replace(',', '.'))
-    y = array_text_lista[0][1]
-    y_float = float(y.replace(',', '.'))
 
-#graficar el bendito punto
-plt.plot(x_float,y_float,marker ="o")
+# Crear una matriz para almacenar los valores de punto flotante
+matriz_float = np.empty_like(array_text_lista, dtype=float)
+
+# Recorrer la matriz y convertir las cadenas en números de punto flotante
+for i in range(array_text_lista.shape[0]):
+    for j in range(array_text_lista.shape[1]):
+        elemento_str = array_text_lista[i, j]
+        elemento_float = float(elemento_str.replace(',', '.'))
+        matriz_float[i, j] = elemento_float
+
+#graficar los puntos recorriendo la matriz matriz_float
+for fila in matriz_float:
+    plt.plot(fila[0],fila[1],marker ="o",color ="green")
+
 plt.show()
