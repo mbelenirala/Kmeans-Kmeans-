e07a3fce8f20d4e52ce5a9a8f16165bb8b15b037
mbelenirala <mbelenirala@gmail.com>
2023-10-14 20:55:13 -0300

    v0.2

--- ControladorKmeans.py
+++ ControladorKmeans.py
@@ -1,5 +1,10 @@
 import numpy as np
 import math
+import ControladorDataset
+
+def mostrar_resultados_kmeans(matriz_estandarizada, k, criterio_parada):
+    centroides, asignaciones = k_means(matriz_estandarizada, k, criterio_parada)
+    ControladorDataset.graficar_resultados(matriz_estandarizada, asignaciones, centroides)
 
 def distanciaEuclidiana(a, b):
     sumaCuadrados = sum((x - y) ** 2 for x, y in zip(a, b))
@@ -9,37 +14,42 @@ def distanciaEuclidiana(a, b):
 # Función para encontrar el centroide más cercano
 def centroideMasCercano(punto, centroides):
     distancia_minima = float('inf')
-    centroide_mas_cercano = None
+    centroideCercano = None
 
     for i, centroide in enumerate(centroides):
         distancia = distanciaEuclidiana(punto, centroide)
         if distancia < distancia_minima:
             distancia_minima = distancia
-            centroide_mas_cercano = i
+            centroideCercano = i
 
-    return centroide_mas_cercano
+    return centroideCercano
 
 def k_means(datos, k, criterio_parada):
 
-    #VER capaz hay que definir otra manera de iniciar aleatoriamente segun lo planteado en el tp
     centroides = datos[np.random.choice(range(len(datos)), k, replace=False)]
     asignaciones_previas = np.empty(0)
-    
+
     iteracion = 1
+    paso_a_paso = f'Centroides iniciales:\n'
+    for i, centroide in enumerate(centroides):
+        paso_a_paso += f'Centroide {i + 1}: {centroide}\n'
+
     while True:
-        print(f'Iteración {iteracion}:')
+        print (f'Iteración {iteracion}')
+        paso_a_paso += f'Iteración {iteracion}\n'
         
         # Asigna cada punto al centroide más cercano
         asignaciones = []
         for punto in datos:
-            centroide_mas_cercano = centroideMasCercano(punto, centroides)
-            asignaciones.append(centroide_mas_cercano)
+            centroideCercano = centroideMasCercano(punto, centroides)
+            asignaciones.append(centroideCercano)
         
         asignaciones = np.array(asignaciones)
         
         # Criterio de parada 1: No haya ninguna reasignación de datos a diferentes clusters
         if criterio_parada == 1 and np.array_equal(asignaciones, asignaciones_previas):
-            print("Criterio de parada 1 alcanzado: No hay reasignaciones.")
+            print ("Criterio de parada 1 alcanzado: No hay reasignaciones.\n")
+            paso_a_paso += "Criterio de parada 1 alcanzado: No hay reasignaciones.\n"
             break
         
         # Actualizar los centroides como el promedio de los puntos asignados
@@ -53,14 +63,16 @@ def k_means(datos, k, criterio_parada):
                 nuevos_centroides.append(centroides[i])
         
         nuevos_centroides = np.array(nuevos_centroides)
-        
-        print("Centroides actualizados:")
+        #print ("Centroides actualizados:\n")
+        #paso_a_paso += "Centroides actualizados:\n"
         for i, centroide in enumerate(nuevos_centroides):
             print(f'Centroide {i + 1}: {centroide}')
+            paso_a_paso += f'Centroide {i + 1}: {centroide}\n'
         
         # Criterio de parada 2: No haya ningún cambio en los centroides
         if criterio_parada == 2 and np.all(centroides == nuevos_centroides):
             print("Criterio de parada 2 alcanzado: No hay cambios en los centroides.")
+            paso_a_paso += "Criterio de parada 2 alcanzado: No hay cambios en los centroides.\n"
             break
         
         centroides = nuevos_centroides
@@ -68,5 +80,9 @@ def k_means(datos, k, criterio_parada):
         asignaciones_previas = asignaciones.copy()
         
         iteracion += 1
-    
-    return centroides, asignaciones
+
+    return centroides, asignaciones, paso_a_paso
+
+def mostrar_resultados_kmeans_plus_plus(matriz_estandarizada, k, criterio_parada):
+    # Por ahora, no hacemos nada en esta función
+    pass
--- main.py
+++ main.py
@@ -1,40 +1,127 @@
+import tkinter as tk
+from tkinter import ttk
 import ControladorDataset
 import ControladorKmeans
-import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+def cerrar_programa():
+    root.quit()
+    root.destroy()
 
-# Definir el número del dataset que deseas cargar (1, 2 o 3)
-dataset = 1
-csv = f'./dataset/dataset_{dataset}.csv'
-matriz = ControladorDataset.cargar_dataset(csv)
-
-
-#VER: si es necesario estandarizar los valores del dataset (segun la teoria recomendaba estadarizar)
-min_val = np.min(matriz, axis=0)
-max_val = np.max(matriz, axis=0)
-matriz_estandarizada = (matriz - min_val) / (max_val - min_val)
-
-#Valores de k a considerar (de 2 a 5)
-valores_k = [2, 3, 4, 5]
-
-# Criterio de parada (1: No haya ninguna reasignación, 2: No haya ningún cambio de centroides)
-# VER: criterio de parada 3
-criterioParada = 1  
-
-# print("Matriz estandarizada:")
-# for fila in matriz_estandarizada:
-#     print(fila)
-
-for k in valores_k:
-    centroides, asignaciones = ControladorKmeans.k_means(matriz_estandarizada, k, criterioParada)
+def graficar():
+    dataset = nroDataset.get()
+    k = k_var.get()
+    criterioParada = int(nroCritero.get())
+    csv = f'./dataset/dataset_{dataset}.csv'
+    matriz = ControladorDataset.cargar_dataset(csv)
+    centroides, asignaciones, pasos = ControladorKmeans.k_means(matriz, k, criterioParada)
     
-    inercia = 0
-    for i in range(len(matriz_estandarizada)):
-        punto = matriz_estandarizada[i]
-        cluster_id = asignaciones[i]
-        centroide = centroides[cluster_id]
-        distancia_cuadrada = sum((punto - centroide)**2)
-        inercia += distancia_cuadrada
-    
-    print(f'Clusters para k={k}, Inercia: {inercia}')
-    ControladorDataset.graficar_resultados(matriz_estandarizada, asignaciones, centroides)
+    # Muestra los pasos en el área de texto
+    texto_area.delete(1.0, tk.END)  # Borra el contenido anterior
+    texto_area.insert(tk.END, pasos)
+
+    graficoKmeans(matriz, centroides, asignaciones)
+
+def graficoDataset(dataset):
+    for widget in datasetFrame.winfo_children():
+        widget.destroy()
+
+    # Grafica el dataset seleccionado
+    csv = f'./dataset/dataset_{dataset}.csv'
+    matriz = ControladorDataset.cargar_dataset(csv)
+    plt.figure(figsize=(5, 5))
+    plt.scatter(matriz[:, 0], matriz[:, 1], c='blue', marker='o')
+    plt.title(f'Dataset {dataset}')
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    dataset_canvas = FigureCanvasTkAgg(plt.gcf(), master=datasetFrame)
+    dataset_canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+    # Borrar gráfico
+    for widget in kmeansFrame.winfo_children():
+        widget.destroy()
+
+def graficoKmeans(matriz, centroides, asignaciones):
+    # Borrar gráfico
+    for widget in kmeansFrame.winfo_children():
+        widget.destroy()
+
+    # Grafica los resultados del K-Means
+    plt.figure(figsize=(5, 5))
+    plt.scatter(matriz[:, 0], matriz[:, 1], c=asignaciones, marker='o')
+    plt.scatter(centroides[:, 0], centroides[:, 1], c='red', marker='x', s=100, label='Centroides')
+    plt.title('Resultados K-Means')
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    kmeans_canvas = FigureCanvasTkAgg(plt.gcf(), master=kmeansFrame)
+    kmeans_canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+root = tk.Tk()
+root.title("K-Means Visualizer")
+root.protocol("WM_DELETE_WINDOW", cerrar_programa)
+
+mainFrame = ttk.Frame(root)
+mainFrame.pack(padx=20, pady=20)
+
+datasetFrame = tk.LabelFrame(mainFrame, text="Dataset",font=("Arial", 14))
+datasetFrame.grid(row=0, column=0)
+
+kmeansFrame = tk.LabelFrame(mainFrame, text="Resultados K-Means",font=("Arial", 14))
+kmeansFrame.grid(row=0, column=1)
+
+controlesFrame = tk.LabelFrame(mainFrame)
+controlesFrame.grid(row=1, column=0, columnspan=2)
+
+dataset_label = tk.Label(controlesFrame, text="Seleccionar Dataset:",font=("Arial", 14))
+dataset_label.grid(row=0, column=0)
+
+nroDataset = tk.StringVar()
+nroDataset.set("1")  # Por defecto, se grafica el 1
+
+# Seleccionar dataset 
+def seleccionarDataset():
+    seleccion = nroDataset.get()
+    graficoDataset(seleccion)  
+
+dataset1_button = ttk.Radiobutton(controlesFrame, text="Dataset 1", variable=nroDataset, value="1", command=seleccionarDataset)
+dataset1_button.grid(row=0, column=1)
+
+dataset2_button = ttk.Radiobutton(controlesFrame, text="Dataset 2", variable=nroDataset, value="2", command=seleccionarDataset)
+dataset2_button.grid(row=0, column=2)
+
+dataset3_button = ttk.Radiobutton(controlesFrame, text="Dataset 3", variable=nroDataset, value="3", command=seleccionarDataset)
+dataset3_button.grid(row=0, column=3)
+
+k_label = tk.Label(controlesFrame, text="Valor de K:",font=("Arial", 14))
+k_label.grid(row=1, column=0)
+k_var = tk.IntVar()
+k_dropdown = ttk.Combobox(controlesFrame, textvariable=k_var, values=[2, 3, 4, 5], state="readonly")
+k_dropdown.grid(row=1, column=1)
+k_dropdown.set(2)
+
+criterio_label = tk.Label(controlesFrame, text="Criterio de Parada:",font=("Arial", 14))
+criterio_label.grid(row=2, column=0)
+nroCritero = tk.StringVar()
+nroCritero.set("1")  
+
+criterioOpcion1 = ttk.Radiobutton(controlesFrame, text="No haya más asignaciones", variable=nroCritero, value="1")
+criterioOpcion1.grid(row=2, column=1)
+
+criterioOpcion2 = ttk.Radiobutton(controlesFrame, text="No haya más cambios en los centroides", variable=nroCritero, value="2")
+criterioOpcion2.grid(row=2, column=2)
+
+botonGraficar = ttk.Button(controlesFrame, text="Graficar Resultados", command=graficar, style="TButton")
+botonGraficar.grid(row=3, column=0, columnspan=2)
+
+# Configurar el estilo del botón directamente
+ttk.Style().configure("TButton", font=("Arial", 12), padding=(10, 5))
+ttk.Style().map("TButton", foreground=[("active", "blue")], background=[("active", "lightgray")])
+
+# Agregar un área de texto para mostrar los pasos
+texto_area = tk.Text(controlesFrame, height=10, width=40)
+texto_area.grid(row=4, column=0, columnspan=3)
+
+graficoDataset("1")
+
+root.mainloop()
